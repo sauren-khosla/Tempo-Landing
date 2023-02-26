6b9f1155899ce86e2ebbf64377976f7b2d4d9fe6
Sauren Khosla <sksauren@gmail.com>
2023-02-26 15:28:02 -0800

    responsive design, adjust submit button

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 const form = document.querySelector('form');
+const submitButton = document.querySelector('button[type="submit"]');
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const firstNameInput = document.querySelector('#first-name');
-  const firstNameValue = firstNameInput.value;
+    const firstNameInput = document.querySelector('#first-name');
+    const firstNameValue = firstNameInput.value;
 
-  const lastNameInput = document.querySelector('#last-name');
-  const lastNameValue = lastNameInput.value;
+    const lastNameInput = document.querySelector('#last-name');
+    const lastNameValue = lastNameInput.value;
 
-  const emailInput = document.querySelector('#email');
-  const emailValue = emailInput.value;
+    const emailInput = document.querySelector('#email');
+    const emailValue = emailInput.value;
 
-  const data = {
+    const data = {
     firstName: firstNameValue,
     lastName: lastNameValue,
     email: emailValue,
-  };
-
-  fetch('https://us-west1-tempo-4c412.cloudfunctions.net/add-to-waitlist', {
+    };
+    
+    fetch('https://us-west1-tempo-4c412.cloudfunctions.net/add-to-waitlist', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
     },
     mode: 'cors',
     body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    
+    // Disable the button and change its text
+    submitButton.disabled = true;
+    submitButton.textContent = 'You\'ve been added!';
+    submitButton.classList.add('disabled');
 });
