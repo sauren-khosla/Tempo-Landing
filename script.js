d25a3c494a35be093f1efcbdaa525fd2b88fb1bc
Sauren Khosla <sksauren@gmail.com>
2023-02-26 14:20:27 -0800

    add cors

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ form.addEventListener('submit', (event) => {
     headers: {
       'Content-Type': 'application/json'
     },
+    mode: 'cors',
     body: JSON.stringify(data)
   })
   .then(response => response.json())
