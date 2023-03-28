e6f0eb750d974505ab388074691fb4d390d5ee6b
Sauren Khosla <sksauren@gmail.com>
2023-03-27 21:58:04 -0700

    new fields

--- script.js
+++ script.js
@@ -13,10 +13,24 @@ form.addEventListener('submit', (event) => {
     const emailInput = document.querySelector('#email');
     const emailValue = emailInput.value;
 
+    const source = document.querySelector("#source");
+    const sourceValue = source.value;
+
+    const friend1 = document.querySelector("#friend-1");
+    const friend2 = document.querySelector("#friend-1");
+    const friend3 = document.querySelector("#friend-1");
+    const friend1Val = friend1.value;
+    const friend2Val = friend2.value;
+    const friend3Val = friend3.value;
+
     const data = {
-    firstName: firstNameValue,
-    lastName: lastNameValue,
-    email: emailValue,
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        email: emailValue,
+        source: sourceValue,
+        friend1: friend1Val,
+        friend2: friend2Val,
+        friend3: friend3Val,
     };
     
     fetch('https://us-west1-tempo-4c412.cloudfunctions.net/add-to-waitlist', {
